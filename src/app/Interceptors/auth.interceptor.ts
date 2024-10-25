4adbee634c864a25c8fc4aac36ed133509f08c7d
Caleb Nasio <nascal3@gmail.com>
2024-10-25 11:50:12 +0300

    Fixed interceptor bug

--- src/app/Interceptors/auth.interceptor.ts
+++ src/app/Interceptors/auth.interceptor.ts
@@ -1,15 +1,23 @@
 import { HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (
   req: HttpRequest<any>,
   next: HttpHandlerFn
 ): Observable<HttpEvent<any>> => {
-  const token = localStorage.getItem('authToken');
+  const authService = inject(AuthService);
+  const token = localStorage.getItem('authToken');  // Or use authService.getToken()
 
-  const cloned = token
-    ? req.clone({ headers: req.headers.set('Authorization', `Bearer ${token}`) })
-    : req;
+  const clonedReq = req.clone({
+    headers: req.headers.set(
+      'Authorization',
+      token
+        ? `Bearer ${token}`
+        : 'Basic Qm9tYVBvcnRhbENsaWVudDpjYmZiZDBhYi0yODc2LTQ0MmItYTNjOC04YWVkOTYzMmJhODM='
+    ),
+  });
 
-  return next(cloned);
+  return next(clonedReq);
 };
